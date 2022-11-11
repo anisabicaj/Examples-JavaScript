7043e0a8e5762a9703568bc431ed523d30f696ac
Anisa Bicaj <anisa.bicaj@student.uni-pr.edu>
2022-11-11 23:10:22 +0100

    Examples-JavaScript

---
+++ homework.js
@@ -0,0 +1,65 @@
+//1 Request a user’s name and display the response “Hello, [name]”.
+//2 Request a user’s year of birth, count his age, and display the result. Store the current year as a constant.
+//3 Request a length of a side of a square from a user and display the perimeter of the square.
+//4 Request a radius of a circle and display the area of such a circle.
+//5 Request a distance in km between the two cities from a user, and the time they want to cover it within. Count the speed needed to be on time.
+//6 Create a currency converter. A user types in USD and the program converts them to EUR. The exchange rate should be stored as a constant.
+//7 A user states a flash drive memory in GB. The program has to count how many 820MB files can be stored on this flash drive.
+//8 A user types in an amount of money in a wallet and the price of one chocolate bar. The program counts how many chocolate bars the user can buy, and how much change will he/she have left.
+//9 The user types in a sum of bank deposits for 2 months with a yearly interest rate of 5%. The program counts the sum of interest.
+
+
+// 1
+// let name=prompt('Name:');
+// let templateLiteral=`Hello ${name}`;
+// console.log(templateLiteral);
+
+
+// 2
+// let birthYear=prompt('Year of birth:')*1;
+// const currentYear=2022;
+// console.log(currentYear-birthYear);
+
+
+//3
+// let square=prompt('Add side length:')*1;
+// console.log(square * 4);
+
+
+//4
+// let radius=prompt('Add radius:')*1;
+// const pi=3.14;
+// console.log(2*pi*radius);
+
+
+//5
+// let distance=prompt('Add distance between two cities:')*1;
+// let time=prompt('Add time:')*1;
+// console.log(distance/time);
+
+
+//6
+// let dollar=prompt('Enter value in dollars?')*1;
+// const exchange=1.2;
+// console.log(dollar * exchange);
+
+
+//7
+// let memory_gb = prompt('Enter the flash drive memory in GB: ')*1;
+// let storage = (memory_gb*1000)/820;
+// console.log('Flash drive can store:'+' '+ storage+' '+'820MB files ');
+
+
+//8
+// let money = prompt('The value of money that you have:')*1;
+// let chocolate = prompt('Price of a chocolate: ')*1;
+// console.log('You can buy:'+' '+money/chocolate+' '+ 'chocolates');
+
+
+//9
+// let deposit=prompt('Bank deposit:')*1;
+// let time=prompt('Time:')*1;
+// const rate=5;
+// console.log((deposit*time*rate)/100);
+
+
